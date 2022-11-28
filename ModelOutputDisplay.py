7bc432489a9028bf9d8e09dab867a663b3c1b9ed
alina.munir <alina.munir@studium.uni-hamburg.de>
2022-11-28 18:47:03 +0100

    just to visualize the cube at predicted outputs by model in testingModelOutputs.blend file

---
+++ ModelOutputDisplay.py
@@ -0,0 +1,8 @@
+import bmesh
+import bpy
+from bpy.props import BoolProperty
+import numpy as np
+import time
+from math import radians
+
+bpy.ops.mesh.primitive_cube_add(size=0.1, location=(-0.2088, -0.2531, -1.5381))
